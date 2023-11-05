fc57a12b1de9bf939adee140aaa3db85592ae06b
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2023-11-05 11:46:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,8 +28,8 @@ rm -rf feeds/luci/applications/luci-app-upnp
 rm -rf feeds/luci/applications/luci-app-wrtbwmon
 git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 
-git clone https://github.com/kevinCSDN/sirpdboy-package/luci-theme-btmob package/luci-theme-btmob
-git clone https://github.com/kevinCSDN/sirpdboy-package/luci-theme-upnp package/luci-theme-upnp
+git clone --depth=1 https://github.com/kevinCSDN/sirpdboy-package/luci-theme-btmob package/luci-theme-btmob
+git clone --depth=1 https://github.com/kevinCSDN/sirpdboy-package/luci-theme-upnp package/luci-theme-upnp
 svn export https://github.com/haiibo/packages/trunk/luci-theme-atmaterial package/luci-theme-atmaterial
 svn export https://github.com/haiibo/packages/trunk/luci-theme-opentomcat package/luci-theme-opentomcat
 svn export https://github.com/haiibo/packages/trunk/luci-theme-netgear package/luci-theme-netgear
